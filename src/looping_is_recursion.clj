0985751b0e7f4d164e3fe5d8f59d9cccdae4770c
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2013-10-27 13:19:17 +0200

    kaikkeni annoin

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,27 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [temp (fn [base exp power]
+               (cond
+                (zero? base) 0
+                (zero? exp) power
+                :else (recur base (dec exp) (* power base))))]
+    (temp base exp 1)))
 
 (defn last-element [a-seq]
-  ":(")
+  (if (empty? (rest a-seq))
+    (first a-seq)
+    (recur (rest a-seq))))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (let [temp (fn [seq1 seq2]
+                 (cond
+                  (and (empty? seq1)(empty? seq2)) true
+                  (and (not(empty? seq2)) (empty? seq1) ) false
+                  (and (not(empty? seq1)) (empty? seq2)) false
+                  (= (first seq1) (first seq2)) (recur (rest seq1) (rest seq2))
+                  :else false))]
+    (temp seq1 seq2)))
 
 (defn find-first-index [pred a-seq]
   ":(")
